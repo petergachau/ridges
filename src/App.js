cee6cce7dffda91e3c4156e566c03c4c9f5843f5
petergachau <petergachau57@gmail.com>
2022-04-01 09:12:48 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import pages
 import Home from "./pages/Home";
-import logo from './logo/logo.png'
+
 import Whatsapp from './pages/Whatsapp'
 import Text from './pages/Text'
-import About from "./pages/About";
-import SingleCocktail from "./pages/SingleCocktail";
+
+
 import Error from "./pages/Error";
 // import components
 import Navbar from "./components/Navbar";
-import CocktailList from "./components/CocktailList";
+
 import Email from './pages/Email'
 import Footer from "./components/Footer";
-import { Link } from "react-router-dom";
+
 function App() {
   return (
     <>
--- src/pages/Whatsapp.js
+++ src/pages/Whatsapp.js
@@ -1,38 +1,9 @@
-import React, { useState } from "react";
-import axios from "axios";
-var QRCode = require("qrcode.react");
+import React from 'react'
 
 const Whatsapp = () => {
-  const [loading, setLoading] = useState(false);
-  const [phone, setPhone] = useState("");
-  const [msg, setMessage] = useState("");
-  const [qrcode, setQRCode] = useState(false);
-
-  const getQRCode = async () => {
-    setLoading(true);
-    const res = await axios.post("/api", { phone, msg });
-    setQRCode(res.data);
-    setLoading(false);
-  };
-
   return (
-    <>
-    <div>
-      Phone Number:
-      <input value={phone} onChange={(e) => setPhone(e.target.value)} />
-      Message:
-      <input value={msg} onChange={(e) => setMessage(e.target.value)} />
-      <button onClick={getQRCode}>Get QRCode</button>
-      {!loading && qrcode && (
-        <div style={{ margin: "100px" }}>
-          <QRCode value={qrcode} />
-        </div>
-      )}
-      {loading && "Waiting for QRCode..."}
-    </div>
-    <div>Text us direct on 0757198515</div>
-    </>
-  );
-};
+    <div>Whatsapp</div>
+  )
+}
 
-export default Whatsapp;
+export default Whatsapp
